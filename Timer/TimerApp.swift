7d6977dee741a8a4f3ef9ae121fa4de7f884f589
jaewak <jaewan.0907@gmail.com>
2024-12-21 23:37:36 -0500

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Jaewan Kim on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
