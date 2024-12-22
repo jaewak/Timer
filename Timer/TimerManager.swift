142019a9c9f2852380565a8595b10331522b7942
jaewak <jaewan.0907@gmail.com>
2024-12-21 23:54:25 -0500

    create simple manager class

--- Timer/TimerManager.swift
+++ Timer/TimerManager.swift
@@ -11,6 +11,25 @@ class TimerManager: ObservableObject {
     @Published var timeLeft: Int = 300
     
     private var isRunning: Bool = false
+    private var timer: Timer?
     
+    func startTimer(timeInSeconds: Int) {
+        isRunning = true
+        
+        timeLeft = timeInSeconds
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in 
+            self.timeLeft -= 1
+            
+            if self.timeLeft == 0 {
+                self.timer?.invalidate()
+            }
+        }
+    }
     
+    func stopTimer() {
+        isRunning = false
+        timeLeft = 0
+        timer?.invalidate()
+    }
 }
