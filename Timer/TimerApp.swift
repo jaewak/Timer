b792315b8308d69d8a58cd10b9b18054550089ed
jaewak <jaewan.0907@gmail.com>
2024-12-21 23:38:59 -0500

    initial commit

--- Timer/TimerApp.swift
+++ Timer/TimerApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct TimerApp: App {
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra ("Timer", systemImage: "timer") {
             ContentView()
         }
     }
